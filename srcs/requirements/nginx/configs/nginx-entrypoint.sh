235ae6ca73364a46dcfee903bdd210272242356d
Jamshidbek Ergashev <jergashe@student.42heilbronn.de>
2023-10-11 08:10:22 +0200

    mariadb alpine test

--- srcs/requirements/nginx/configs/nginx-entrypoint.sh
+++ srcs/requirements/nginx/configs/nginx-entrypoint.sh
@@ -15,7 +15,7 @@ if [ ! -f /etc/nginx/sites-enabled/nginx.conf ]; then
 	unlink /etc/nginx/sites-enabled/default
 
 	# Replace '$DOMAIN_NAME' with the actual domain name in the nginx configuration
-	# sed -ie s/'$DOMAIN_NAME'/$DOMAIN_NAME/g /etc/nginx/sites-available/nginx.conf
+	sed -ie s/'$DOMAIN_NAME'/$DOMAIN_NAME/g /etc/nginx/sites-available/nginx.conf
 
 	# Create a symlink to your custom Nginx configuration
 	# "sites-enabled" folder stores symlinks to the configurations you want Nginx to actively use.
--- srcs/requirements/wordpress/configs/docker-entrypoint.sh
+++ srcs/requirements/wordpress/configs/docker-entrypoint.sh
@@ -32,4 +32,4 @@ if [[ ! -f /var/www/html/wp-config.php ]]; then
 
 fi
 
-php-fpm7.3 -F
+# php-fpm7.3 -F

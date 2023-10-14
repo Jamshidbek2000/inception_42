5f39658e0a824954c82e27de598db557c3254c0e
Jamshidbek Ergashev <jergashe@student.42heilbronn.de>
2023-10-14 09:59:21 +0200

    after inception started working on Mac. Ready for test1 on VM

--- srcs/requirements/wordpress/configs/script.sh
+++ srcs/requirements/wordpress/configs/script.sh
@@ -15,9 +15,8 @@ if [ ! -f /var/www/html/wp-config.php ]; then
 	wp core download --allow-root
 	wp config create --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_USER_PWD --dbhost=$DB_HOST --allow-root
 
-	# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!   CHANGE ENV VAR --url=
 	# Install WordPress and create a user
-	wp core install --url=localhost/ --title=$WP_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
+	wp core install --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
 	wp user create $WP_USER $WP_EMAIL --role=author --user_pass=$WP_PWD --allow-root --porcelain
 
 	# Move PHP-FPM configuration
